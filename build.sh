8bfd8f317ac962bfcd1e11d221ccd6c9925528e2
Louis-2024 <weiheng.xiao@mail.mcgill.ca>
2025-09-05 11:58:30 -0400

    [Fix] Customized build.sh scripts

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-CC=clang-12 CXX=clang++-12 scons build/X86_LC_MSI/gem5.fast --ignore-style -j18 --default=X86_LC_MSI --mold-linker
+rm -rf build/.scons_config build/sconsign build/variables/ build/variables.global
+ 
+CC=clang-12 CXX=clang++-12 \
+scons -Q --config=force \
+    PYTHON_CONFIG=/usr/bin/python3.8-config \
+    build/X86_LC_MSI/gem5.fast --ignore-style -j8 --default=X86_LC_MSI
