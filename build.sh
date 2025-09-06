0760cd71e45b343c97280ddbd0e49c77d5071368
Louis-2024 <weiheng.xiao@mail.mcgill.ca>
2025-09-05 20:19:56 -0400

    [Fix] Add linker back to build.sh scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 rm -rf build/.scons_config build/sconsign build/variables/ build/variables.global
  
-CC=clang-12 CXX=clang++-12 \
-scons -Q --config=force \
-    PYTHON_CONFIG=/usr/bin/python3.8-config \
-    build/X86_LC_MSI/gem5.fast --ignore-style -j8 --default=X86_LC_MSI
+scons --config=force -Q \
+  PYTHON_CONFIG=/usr/bin/python3.8-config \
+  CC=clang-12 CXX=clang++-12 \
+  build/X86_LC_MSI/gem5.fast \
+  --ignore-style -j8 --default=X86_LC_MSI --gold-linker
