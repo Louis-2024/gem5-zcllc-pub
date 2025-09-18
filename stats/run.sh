38f6b7c71cb3152f8189d6f58f7c6d5d96beed68
Louis-2024 <weiheng.xiao@mail.mcgill.ca>
2025-09-18 10:58:48 +0000

    [update] changed config to call SelectLine()

--- stats/run.sh
+++ stats/run.sh
@@ -1,3 +1,3 @@
 cd ..
 rm -rf m5out
-PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.fast configs/xyz/simple_ruby.py  --ncore 4 --wc --use-ziv --use-vi --l1-size 4kB --l2-size 16kB --l3-size 2MB --slot-width 128 --ruby-test
+PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.fast configs/xyz/simple_ruby.py  --ncore 8 --wc --use-ziv --use-vi --l1-size 8kB --l2-size 64kB --l3-size 1MB --slot-width 128 --ruby-test
