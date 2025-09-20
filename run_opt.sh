1fdb63abefdddf14ce490f1eabee62642a856d96
Louis_2024 <weiheng.xiao@mail.mcgill.ca>
2025-09-20 20:48:17 +0000

    BASELINE CONFIGURATION

--- run_opt.sh
+++ run_opt.sh
@@ -2,10 +2,10 @@ rm -rf m5out
 
 # kernal test
 
-PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.opt --debug-flags=ZIVCache --debug-file=ziv.log --debug-start=0 \
-    configs/xyz/simple_ruby.py --wc --ncore 4 --wc --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
-    --program /gem5/Splash-3/codes/kernels/cholesky/CHOLESKY --cwd /gem5/Splash-3/codes/kernels/cholesky --args "-p4 -B32 -C65536" \
-    --input-file /gem5/Splash-3/codes/kernels/cholesky/inputs/tk14.O
+# PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.opt --debug-flags=ZIVCache --debug-file=ziv.log --debug-start=0 \
+#     configs/xyz/simple_ruby.py --wc --ncore 4 --wc --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
+#     --program /gem5/Splash-3/codes/kernels/cholesky/CHOLESKY --cwd /gem5/Splash-3/codes/kernels/cholesky --args "-p4 -B32 -C65536" \
+#     --input-file /gem5/Splash-3/codes/kernels/cholesky/inputs/tk14.O
 
 # PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.opt --debug-flags=ZIVCache --debug-file=ziv.log --debug-start=0 \
 #     configs/xyz/simple_ruby.py --wc --ncore 4 --wc --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
@@ -13,6 +13,6 @@ PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.o
 
 # app test
 
-# PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.opt --debug-flags=ZIVCache --debug-file=ziv.log --debug-start=0 \
-#     configs/xyz/simple_ruby.py --wc --ncore 4 --wc --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
-#     --program /gem5/Splash-3/codes/apps/fmm/FMM --cwd /gem5/Splash-3/codes/apps/fmm --input-file /gem5/Splash-3/codes/apps/fmm/inputs/input.4.256
+PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.opt --debug-flags=ZIVCache --debug-file=ziv.log --debug-start=0 \
+    configs/xyz/simple_ruby.py --wc --ncore 4 --wc --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
+    --program /gem5/Splash-3/codes/apps/fmm/FMM --cwd /gem5/Splash-3/codes/apps/fmm --input-file /gem5/Splash-3/codes/apps/fmm/inputs/input.4.256
