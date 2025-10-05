994ecbf831d666bd1c6e76d377a40bb789bc25eb
Louis_2024 <weiheng.xiao@mail.mcgill.ca>
2025-10-05 12:14:40 +0000

    update: added stats for advanced-lru

--- run.sh
+++ run.sh
@@ -2,18 +2,15 @@ rm -rf m5out
 
 # kernal test
 
-# PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.fast \
-#     configs/xyz/simple_ruby.py --wc --ncore 4 --wc --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
-#     --program /gem5/Splash-3/codes/kernels/cholesky/CHOLESKY --cwd /gem5/Splash-3/codes/kernels/cholesky --args "-p4 -B32 -C65536" \
-#     --input-file /gem5/Splash-3/codes/kernels/cholesky/inputs/tk14.O
+PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.fast \
+    configs/xyz/simple_ruby.py --wc --ncore 4 --wc --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
+    --program /gem5/Splash-3/codes/kernels/cholesky/CHOLESKY --cwd /gem5/Splash-3/codes/kernels/cholesky --args "-p4 -B32 -C65536" \
+    --input-file /gem5/Splash-3/codes/kernels/cholesky/inputs/tk14.O
 
 # PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.fast \
 #     configs/xyz/simple_ruby.py --wc --ncore 4 --wc --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
 #     --program /gem5/Splash-3/codes/kernels/lu/contiguous_blocks/LU --cwd /gem5/Splash-3/codes/kernels/lu/contiguous_blocks --args "-p4 -n256 -b16"
 
-# PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.fast \
-#     configs/xyz/simple_ruby.py --ncore 4 --wc --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
-#     --program /gem5/Splash-3/codes/kernels/fft/FFT --cwd /gem5/Splash-3/codes/kernels/fft --args "-p4 -m16 -n16 -l4"
 
 # app test
 
@@ -21,7 +18,7 @@ rm -rf m5out
 #     configs/xyz/simple_ruby.py --wc --ncore 4 --wc --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
 #     --program /gem5/Splash-3/codes/apps/fmm/FMM --cwd /gem5/Splash-3/codes/apps/fmm --input-file /gem5/Splash-3/codes/apps/fmm/inputs/input.4.256
 
-PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.fast \
-    configs/xyz/simple_ruby.py --wc --ncore 4 --wc --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
-    --program /gem5/Splash-3/codes/apps/ocean/contiguous_partitions/OCEAN --cwd /gem5/Splash-3/codes/apps/ocean/contiguous_partitions \
-    --args "-p4 -n130"
+# PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.fast \
+#     configs/xyz/simple_ruby.py --wc --ncore 4 --wc --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
+#     --program /gem5/Splash-3/codes/apps/ocean/contiguous_partitions/OCEAN --cwd /gem5/Splash-3/codes/apps/ocean/contiguous_partitions \
+#     --args "-p4 -n130"
