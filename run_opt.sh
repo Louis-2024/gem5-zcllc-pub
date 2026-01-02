2008033c76b6a4bc403f03fb947e1411912451c9
Louis_2024 <weiheng.xiao@mail.mcgill.ca>
2026-01-02 19:54:40 +0000

    update: added test commands for heavyweight benchmarks

--- run_opt.sh
+++ run_opt.sh
@@ -16,7 +16,7 @@ PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.o
 #     --program /gem5/Splash-3/codes/apps/ocean/contiguous_partitions/OCEAN --cwd /gem5/Splash-3/codes/apps/ocean/contiguous_partitions \
 #     --args "-p4 -n130"
 
-# slow tests
+# moderate tests
 
 # PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.opt --debug-flags=ZIVCache --debug-file=ziv.log --debug-start=0 \
 #     configs/xyz/simple_ruby.py --wc --subslot-opt --ncore 4 --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
@@ -29,8 +29,20 @@ PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.o
 #     --program /gem5/Splash-3/codes/apps/barnes/BARNES --cwd /gem5/Splash-3/codes/apps/barnes \
 #     --input-file /gem5/Splash-3/codes/apps/barnes/inputs/n8k-p4
 
+# slow tests
+
+# PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.opt --debug-flags=ZIVCache --debug-file=ziv.log --debug-start=0 \
+#     configs/xyz/simple_ruby.py --wc --subslot-opt --ncore 4 --wc --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
+#     --program /gem5/Splash-3/codes/apps/fmm/FMM --cwd /gem5/Splash-3/codes/apps/fmm \
+#     --input-file /gem5/Splash-3/codes/apps/fmm/inputs/input.4.16384
+
+# PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.opt --debug-flags=ZIVCache --debug-file=ziv.log --debug-start=0 \
+#     configs/xyz/simple_ruby.py --wc --subslot-opt --ncore 4 --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
+#     --program /gem5/Splash-3/codes/apps/barnes/BARNES --cwd /gem5/Splash-3/codes/apps/barnes \
+#     --input-file /gem5/Splash-3/codes/apps/barnes/inputs/n16384-p4
+
 # synthetic test
  
-# PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.opt  --debug-flags=ZIVCache --debug-file=ziv.log --debug-start=0 \
+# PYTHONPATH="$PWD/configs:$PWD/build/X86_LC_MSI/python" ./build/X86_LC_MSI/gem5.opt --debug-flags=ZIVCache --debug-file=ziv.log --debug-start=0 \
 #     configs/xyz/simple_ruby.py --wc --subslot-opt --ruby-test --ncore 4 --use-ziv --use-vi --l1-size 2kB --l2-size 8kB --l3-size 128kB \
 #     --nreq 20000
